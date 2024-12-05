de4e2825279c3e99cdbb5ddc317f069aea614d3f
Wu <jio12138@qq.com>
2024-12-05 23:51:35 +0800

    1.2.0

--- src/.vuepress/navbar.ts
+++ src/.vuepress/navbar.ts
@@ -39,7 +39,8 @@ export default navbar([
         prefix: "papereading/",
         children: [
           {text: "DeepSpeed-MoE 阅读笔记", link: "DS-MoE"},
-          {text: "The Oscars Of AI Theater 阅读笔记", link: "AI_Oscars"}
+          {text: "The Oscars Of AI Theater 阅读笔记", link: "AI_Oscars"},
+          {text: "Marco-o1 阅读笔记", link: "Marco1"},
         ],
       },
       {
